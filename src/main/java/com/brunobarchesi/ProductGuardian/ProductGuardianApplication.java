5430259e8182f8479905b2fad4ee5d1a293dde06
¨brunobarchesii-dev¨ <bruno.barchesii@gmail.com>
2026-06-30 09:57:25 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/com/brunobarchesi/ProductGuardian/ProductGuardianApplication.java
@@ -0,0 +1,13 @@
+package com.brunobarchesi.ProductGuardian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductGuardianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductGuardianApplication.class, args);
+	}
+
+}
